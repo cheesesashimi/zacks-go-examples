5aa7bc8a81931a2eea8252ce5c80707e0cf2d0aa
Zack Zlotnik <zzlotnik@redhat.com>
2023-04-06 10:23:44 -0400

    make test.sh more useful

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# This is a very simple and naive shell-script that traverses the various
+# This is a very simple and naïve shell-script that traverses the various
 # examples directories and ensures that they build. There is a ton of room for
-# improvement here.
-for dir in $(find . -type d -name "[0-9][0-9]-*"); do cd "$dir" && go build . && cd ../..; done
+# improvement here, such as writing a Makefile or similar.
+reporoot="$PWD"
+for file in $(find . -type f -name "main.go"); do
+  example_dir="$(dirname $file)"
+  # We have to cd to each directory since some examples have additional
+  # required files present.
+  cd "$example_dir"
+  go build -o example . && ./example && rm ./example && cd "$reporoot"
+done
